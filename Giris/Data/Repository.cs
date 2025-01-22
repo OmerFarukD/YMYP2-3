fa24f5f75c508f376bad663542fb24430ac313b9
OmerFarukD <89133026+OmerFarukD@users.noreply.github.com>
2025-01-22 13:48:41 +0300

    Giris Projesi Tamamlandı.

--- Giris/Data/Repository.cs
+++ Giris/Data/Repository.cs
@@ -14,8 +14,8 @@ public  class Repository
         new Product{Id=1, Name="Dyson Süpürge",CategoryName="Ev Aletleri",Price=25000,Stock=123},
         new Product{Id=2,Name="Philips Süpürge",CategoryName="Ev Aletleri",Price=15000,Stock=25},
         new Product{Id=3,Name="Arçelik Süpürge",CategoryName="Ev Aletleri",Price=5000,Stock=63},
-        new Product{Id=7,Name="Arçelik Süpürge 3",CategoryName="Ev Aletleri",Price=3250,Stock=63},
-        new Product{Id=4, Name="Dyson Süpürge 1",CategoryName="Ev Aletleri",Price=15000,Stock=120},
+        new Product{Id=7,Name="Arçelik Süpürge 3",CategoryName="Ev Aletleri",Price=3250,Stock=15},
+        new Product{Id=4, Name="Dyson Süpürge 1",CategoryName="Ev Aletleri",Price=15000,Stock=20},
         new Product{Id=5,Name="Philips Süpürge 1",CategoryName="Ev Aletleri",Price=25000,Stock=250},
         new Product{Id=6,Name="Arçelik Süpürge 1",CategoryName="Ev Aletleri",Price=8000,Stock=163}
     };
